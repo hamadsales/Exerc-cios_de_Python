a8b6a044ba86814bf56a162faf0a813e38b7526e
hamadsales <hamadsales@gmail.com>
2026-04-29 16:33:08 -0300

    "exercício 03 finalizado, porém falta aperfeiçoar"

---
+++ 03.py
@@ -0,0 +1,5 @@
+#Faça um programa que peça dois números e imprima a soma:
+num1 = float(input("Digite o número 01: "))
+num2 = float(input("Digite o número 02: "))
+
+print(f'A soma é {num1 + num2}')
